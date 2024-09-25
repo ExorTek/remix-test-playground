db593ed343d85f308798518b554ac35b8cee106e
Memet <exortek979@gmail.com>
2024-09-25 16:39:20 +0300

    Create client-page.jsx

---
+++ app/routes/client-page.jsx
@@ -0,0 +1,10 @@
+function ClientPage(props) {
+    return (
+        <div>
+            <h1>Welcome to the Client Page!</h1>
+            <p>This is a client-only page. It will not be rendered on the server.</p>
+        </div>
+    );
+}
+
+export default ClientPage;
