a36366ea249ad66c22db1ae4392a2afafeffd582
Memet <exortek979@gmail.com>
2024-09-25 16:39:28 +0300

    Create fastify-server.mjs

---
+++ fastify-server.mjs
@@ -0,0 +1,18 @@
+import remixFastify from "./remix-fastify.mjs";
+import { fastify } from "fastify";
+
+const app = fastify();
+
+await app.register(remixFastify)
+
+app.listen({
+    port: 3000,
+    host: "127.0.0.1"
+}, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening on ${address}`);
+})
+
