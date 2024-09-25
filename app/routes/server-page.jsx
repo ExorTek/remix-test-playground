7bf3516a894ef6be7306cf10e10b02be69f8b4d6
Memet <exortek979@gmail.com>
2024-09-25 16:39:23 +0300

    Create server-page.jsx

---
+++ app/routes/server-page.jsx
@@ -0,0 +1,10 @@
+function ServerPage() {
+    return (
+        <div>
+            <h1>Welcome to the Server Page!</h1>
+            <p>This is a server-only page. It will not be rendered on the client.</p>
+        </div>
+    );
+}
+
+export default ServerPage;
